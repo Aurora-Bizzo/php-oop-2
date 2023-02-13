1b1f297116e13cbf4681d2a0c23ec20c3be20d26
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-02-13 16:29:01 +0100

    completed

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -25,8 +25,8 @@ error_reporting(E_ALL);
         <div class="row">
             <?php foreach($products as $product){ ?>
                 <div class="col-sm-4">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="<?php echo $product->image; ?>" alt="Card image cap">
+                    <div class="card my-4" style="width: 18rem;">
+                        <img class="card-img-top p-2" src="<?php echo $product->image; ?>" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">
                                 <?php
